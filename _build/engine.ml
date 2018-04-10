a67065997efd76cc913d288d4f79411d8489613c
Eashaan Kumar <ek485@cornell.edu>
2018-04-10 14:58:36 -0400

    Adding main

---
+++ _build/engine.ml
@@ -0,0 +1,20 @@
+type input = int
+
+type properties = int
+
+exception Init_Failure of string
+
+let init_game prop = 
+    []
+
+let get_input () = 
+    1
+
+let update ste inpt = 
+    []
+
+let render ste = 
+    ()
+
+let game_loop ste run =
+    ste
--- engine.ml
+++ engine.ml
@@ -1,3 +1,5 @@
+open Graphics
+
 type input = int
 
 type properties = int
@@ -12,6 +14,25 @@ let get_input () =
 
 let update ste inpt = 
     []
-    
-val render : State.state -> unit
-val game_loop : State.state -> bool -> State.state
+
+let render ste = 
+    ()
+
+let game_loop ste run =
+    ste
+
+(* [main ()] starts the REPL, which prompts for a game to play.
+ * You are welcome to improve the user interface, but it must
+ * still prompt for a game to play rather than hardcode a game file. *)
+let main () =
+  (*ANSITerminal.(print_string [red]
+    "\n\nWelcome to the 3110 Text Adventure Game engine.\n");
+  play_game (file_prompt ())*)
+  (* TODO: Obtain properties *)
+  (* let prop = blah *)
+  let init_state = init_game prop in
+  let end_state = game_loop init_state true in
+  (* Print out ending game state if necessary before closing *)
+  ()
+
+let () = main ()
