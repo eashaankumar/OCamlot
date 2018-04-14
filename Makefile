7bb6ff9abaf1552e8b4847c9f9236137a94cdaaa
Eashaan Kumar <ek485@cornell.edu>
2018-04-14 14:53:06 -0400

    Switched to js_of_ocaml

--- Makefile
+++ Makefile
@@ -1,22 +1,8 @@
-#test:
-	#ocamlbuild -use-ocamlfind state_test.byte && ./state_test.byte
+all: byte
+	js_of_ocaml cubes.byte
 
-make_editor_errors_go_away:
-	@echo Making editor errors go away!!! 
-
-play:
-	ocamlbuild -use-ocamlfind -pkg graphics -lib unix engine.byte && ./engine.byte
-
-#check:
-#	bash checkenv.sh && bash checktypes.sh
-
-#zip:
-#	zip a2src.zip *.ml* *.json
-	
-#zipcheck:
-#	bash checkzip.sh
+byte:
+	ocamlbuild -use-ocamlfind -pkgs js_of_ocaml-lwt,js_of_ocaml.ppx,lwt.ppx cubes.byte
 
 clean:
 	ocamlbuild -clean
-	#rm -f checktypes.ml
-	#rm -f a2src.zip
--- engine.ml
+++ engine.ml
@@ -1,159 +1,72 @@
-open State
 
-let last_time = ref 0.
-let frame_counter = ref 0
-let second_counter = ref 0.
-let fps = ref 0
+open Js_of_ocaml
+open Js_of_ocaml_lwt
 
-type input = {
-    left_mouse_down:bool
-}
+(* type definitions *)
 
-type properties = {
-    twrs_neutral: (int * int) list;
-    twr_player: (int * int);
-    twr_enemy: (int * int);
-    twr_neutral_size: (int * int);
-    twr_base_size: (int * int);
-    width:int;
-    height:int
-}
+(* fixed vars *)
+let n = 12
 
-exception Init_Failure of string
+let h = 20.
+let w = floor (h *. sqrt 3. /. 2. +. 0.5)
 
-let init_game prop : state = 
-  print_endline "Starting game!";
-  Graphics.set_window_title "Ocamlot";
-  Graphics.open_graph "";
-  Graphics.resize_window (prop.width) (prop.height);
-  let tower_id = ref 0 in
-  (* Create Neutral Towers *)
-  let neutral_twr_array = begin
-    List.fold_left (fun arr pos -> 
-      let new_tower = {id = !tower_id;
-                      pos = pos;
-                      size = prop.twr_neutral_size;
-                      twr_sprite = [];
-                      twr_troops = 0;
-                      twr_troops_max = 20;
-                      twr_team = Neutral} in
-      tower_id := !tower_id + 1;
-      Array.append arr ([|new_tower|])
-    ) [||] (prop.twrs_neutral)
-  end in
-  (* Create Enemy and Player towers *)
-  let base_twr_array = [|{id = !tower_id;
-                          pos = prop.twr_enemy;
-                          size = prop.twr_base_size;
-                          twr_sprite = [];
-                          twr_troops = 0;
-                          twr_troops_max = 50;
-                          twr_team = Enemy};
-                         {id = !tower_id + 1;
-                          pos = prop.twr_player;
-                          size = prop.twr_base_size;
-                          twr_sprite = [];
-                          twr_troops = 0;
-                          twr_troops_max = 50;
-                          twr_team = Player}|] in
-  tower_id := !tower_id + 2;
-  (* Create Init State *)
-  let st = {
-    towers = Array.append neutral_twr_array base_twr_array;
-    num_towers = !tower_id;
-    player_score = 1;
-    enemy_score = 1;
-    movements = [];
-    player_mana = 100;
-    enemy_mana = 100
-  } in
-  last_time := Unix.gettimeofday ();
-  st
+(* vars *)
+let x = ref 0.
 
-let get_input () = 
-  {left_mouse_down=Graphics.button_down ()}
 
-let update st0 inpt = 
-  (* Calculate delta_time and fps *)
-  let delta_time = Unix.gettimeofday () -. !last_time in
-  second_counter := !second_counter +. delta_time;
-  if !second_counter > 1. then (
-    second_counter := 0.;
-    fps := !frame_counter;
-    frame_counter := 0;
-  ) else (
-    second_counter := !second_counter +. delta_time;
-    frame_counter := !frame_counter + 1;
-  );
-  (* Do this as the end of Update *)
-  last_time := Unix.gettimeofday ();
-  if inpt.left_mouse_down then (st0,false) else st0,true
+let update () =
+  print_endline "blah";
+  x := !x +. 1.;
 
-let render st = 
-  Graphics.auto_synchronize false;
-  Graphics.clear_graph ();
-  (* Render Towers *)
-  Array.iter (fun tower -> 
-    let x_pos = (fst (tower.pos)) in
-    let y_pos = (snd (tower.pos)) in
-    let x_size = (fst (tower.size)) in
-    let y_size = (snd (tower.size)) in
-    let tc = begin
-      match (tower.twr_team) with 
-      | Player ->  [|0; 0; 255|]
-      | Enemy -> [|255; 0; 0|]
-      | Neutral -> [|100; 100; 100|]
-    end in
-    Graphics.set_color (Graphics.rgb tc.(0) tc.(1) tc.(2));
-    Graphics.fill_rect x_pos y_pos x_size y_size;
-    (* Troop count text *)
-    Graphics.set_color (Graphics.rgb 0 0 0);
-    Graphics.moveto (x_pos + x_size/2) (y_pos + y_size/2);
-    Graphics.draw_string (string_of_int tower.twr_troops);
-  ) st.towers;
-  (* FPS *)
-  Graphics.set_color (Graphics.rgb 0 0 0);
-  Graphics.moveto (Graphics.size_x () - 30) (Graphics.size_y () - 30);
-  Graphics.draw_string (string_of_int !fps);
-  (**********)
-  Graphics.auto_synchronize true;
-  ()
+(****)
 
-let rec game_loop ste run =
-  if not run then ste 
-  else begin
-    let (new_ste, should_run) = update ste (get_input ()) in
-    render ste;
-    Unix.sleepf 0.01;
-    game_loop new_ste should_run;
-  end
+module Html = Dom_html
+
+let top = Js.string "#a8a8f6"
+let left = Js.string "#d9d9d9"
+let right = Js.string "#767676"
+let edge = Js.string "#000000"
+
+let create_canvas () =
+  let d = Html.window##.document in
+  let c = Html.createCanvas d in
+  c##.width := 1000;
+  c##.height := 800;
+  c
+
+let render ctx canvas =
+  let c = canvas##getContext (Html._2d_) in
+  (* Clear Screen *)
+  c##setTransform 1. 0. 0. 1. 0. 0.;
+  c##clearRect 0. 0. (float canvas##.width) (float canvas##.height);
+  (*c##setTransform 1. 0. 0. 1. 0.5 0.5;*)
+  c##.globalCompositeOperation := Js.string "lighter";
+  c##.fillStyle := Js.string "#0000FF";
+  c##fillRect !x 0. 100. 100.;
+  ctx##drawImage_fromCanvas canvas 0. 0.
   
-let close_game ste = 
-  Graphics.close_graph ();
-  print_endline "Closing Game!";
+let (>>=) = Lwt.bind
 
-;;
+let rec main_loop c c' =
+  Lwt_js.sleep 0.01 >>= fun () ->
+  (*let need_redraw = ref false in
+  for _i = 0 to 99 do
+    need_redraw := update || !need_redraw
+  done;
+  if !need_redraw then render c c';*)
+  update ();
+  render c c';
+  main_loop c c'
 
-(* [main ()] starts the REPL, which prompts for a game to play.
- * You are welcome to improve the user interface, but it must
- * still prompt for a game to play rather than hardcode a game file. *)
-let main () =
-  (* Create temporary property for map, implement property generator later *)
-  let prop = {
-    twrs_neutral=[(100,100);
-            (200,200);
-            (700,500);
-            (600,400)];
-    twr_player=(0, 0);
-    twr_enemy=(768,568);
-    twr_neutral_size = (32,32);
-    twr_base_size = (32, 32);
-    width=800;
-    height=600
-  } in
-  let init_state = init_game prop in
-  let end_state = game_loop init_state true in
-  close_game end_state;
-;;
+let start _ =
+  let c = create_canvas () in
+  let c' = create_canvas () in
+  Dom.appendChild Html.window##.document##.body c;
+  let c = c##getContext Html._2d_ in
+  c##.globalCompositeOperation := Js.string "copy";
+  render c c';
+  ignore (main_loop c c');
+  Js._false
 
-let () = main ()
+let _ =
+Html.window##.onload := Html.handler start
