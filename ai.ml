8eb150c13f2957165f705b45738c27e75e870413
vagrant <vagrant@vm3110-2018sp>
2018-04-20 22:14:37 -0400

    Wrote the monte carlo tree search AI algorithm

--- ai.ml
+++ ai.ml
@@ -10,12 +10,27 @@ module MiniMax_AI = struct
     0.0
       (*Make a good enemy state positive*)
 
+(**
+ * Returns: [swap_allegiance] is the opposite allegiance of
+ *          [side]. In the case of Neutral, Neutral is returned.
+ * Parameters: [side] is of type allegiance
+ *)
   let swap_allegiance side =
     match side with
     | Player -> Enemy
     | Enemy -> Player
     | Neutral -> Neutral
 
+  (**
+   * Returns: [minimax_value] runs the minimax algorithm starting
+   *          at [st] with the other standard minimax parameters.
+   * Parameters: [st] is the beginning state for the algorithm
+   *              [depth] is the number of moves deep to search
+   *              [minV]/[maxV] are float values for alpha-beta pruning
+   *              [side] is an allegiance that determines whether this
+   *                    is a max or a min state
+   *              [original_side] is the side that is to be maximized
+   *)
   let rec minimax_value st depth minV maxV side original_side=
     if (State.gameover st) || depth = 0 then
   		heuristic st side
@@ -79,6 +94,7 @@ module MiniMax_AI = struct
 
   		loop 0 (Array.length moves) value
 
+
   let get_move st =
     let best_move = ref Rest in
     let best_score = ref (0.0 -. max_float) in
@@ -93,6 +109,136 @@ module MiniMax_AI = struct
         best_move := this_move;
     done;
     !best_move
+
+
+end
+
+module MCTS_AI = struct
+  (*Time-step*)
+  let delta = 0.1
+  (*Constant in front of the MTCS value function*)
+  let c = sqrt 2.0
+  (*Number of times to run the algorithm*)
+  let iterations = 100
+
+  (* fraction of wins, number of times played, daughter nodes,
+   * commands that got here, possible commands from here*)
+
+  (*Node (state, command from parent, win_pctg, times_played,
+  daughter_nodes, parent_node, is_max_state)*)
+  type tree =
+    | Leaf of command * float
+    | Node of Types.state * command * float * float * ((tree ref) array) * tree ref * bool
+
+
+  let to_allegiance max_bool =
+    if max_bool then Enemy else Player
+
+  let get_random_command st allegiance =
+    let commands = possible_moves st allegiance in
+    let range = Array.length commands in
+    let index = Random.int range in
+    Array.get commands index
+
+  let rec random_playout st max_bool =
+    if State.gameover st then
+      if st.player_score > st.enemy_score then
+        0.0
+      else
+        1.0
+    else
+      if max_bool then
+        let cm = get_random_command st Enemy in
+        let new_state = new_state_plus_delta st cm delta in
+        random_playout new_state false
+      else
+        let cm = get_random_command st Player in
+        let new_state = new_state_plus_delta st cm delta in
+        random_playout new_state true
+
+  let get_times_sampled t =
+    match t with
+    | Node(st,cm,v,n,children,parent,_) -> n
+    | Leaf _ -> 0.0
+
+  let get_value t is_max =
+    match t with
+    | Leaf (_,v)-> 10000.0
+    | Node(st,cm,v,n,children,parent,_) -> begin
+        if is_max then
+          v +. c *. sqrt (log (get_times_sampled !parent))/.n
+        else
+          (1.0-.v) +. c *.sqrt (log (get_times_sampled !parent))/.n
+      end
+
+  let create_children st allegiance =
+    let moves = possible_moves st allegiance in
+    Array.map (fun cm -> ref (Leaf (cm,10000.0))) moves
+
+  let get_extreme_child node is_max =
+    let func = if is_max then (>) else (<) in
+    let children,is_max =
+      match !node with
+      | Node(_,_,_,_,chldrn,_,is_max_bool) -> chldrn,is_max_bool
+      | Leaf _ -> [||],true in
+    Array.fold_left
+      (fun acc child -> if (func (get_value !child is_max) (get_value !acc is_max)) then child else acc)
+      (Array.get children 0) children
+
+  let update_node node win_loss =
+    match !node with
+    | Node(st,cm,v,n,children,parent,is_max) -> begin
+        node := Node(st,cm,v +. (win_loss-.v)/.(n+.1.0),n+.1.0,children,parent,is_max);
+      end
+    | Leaf _ -> ()
+
+  let rec update_tree node win_loss =
+    match !node with
+    | Node(st,cm,v,n,children,parent,is_max) -> begin
+        update_node node win_loss;
+        update_tree parent win_loss
+      end
+    | Leaf _ -> ()
+
+  let new_node node cm =
+    match !node with
+    | Node(st,cm,v,n,children,parent,is_max) ->
+      let new_st = new_state_plus_delta st cm delta in
+      let rand_play = random_playout new_st is_max in
+      update_tree parent rand_play;
+      ref (Node(new_st, cm, rand_play, 0.0,
+                create_children st (to_allegiance (not is_max)), node, not is_max))
+    | Leaf _ -> node
+
+  let beginning_node st =
+    let children = create_children st Enemy in
+    ref (Node(st,Rest,0.0,0.0,children,ref (Leaf(Rest,1.0)),false))
+
+  let rec add_path t = begin
+    match !t with
+    | Node(st,cm,v,n,children,parent,is_max) -> begin
+        let ex_child = get_extreme_child t is_max in
+        match !ex_child with
+        | Node _ -> add_path ex_child
+        | Leaf (cm,_) -> ex_child := !(new_node t cm)
+      end
+    | Leaf(v,cm) -> ()
+  end
+
+  let create_tree st iters =
+    let root = beginning_node st in
+    let counter = ref 0 in
+    while !counter < iters do
+      add_path root;
+    done;
+    root
+
+  let get_move st =
+    let t = create_tree st iterations in
+    let child = get_extreme_child t true in
+    match !child with
+    | Node(_,cm,_,_,_,_,_) -> cm
+    | Leaf(cm,_) -> cm
 
 
 end
