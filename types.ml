32caed91cf96f11bd8af4fdd0b4a445d93b1c6a4
Eashaan <ek485@cornell.edu>
2018-05-11 13:05:56 -0400

    error

--- types.ml
+++ types.ml
@@ -53,18 +53,24 @@ type movement = {
 }
 
 type effect =
-  | Stun of float (* An attack *)
+  | Stun of float(* An attack *)
   | Regen_incr of float (* A buff if > 1.0, an attack if < 1.0. *)
   | Kill of int
 
-type skill_side =
-  | Buff
-  | Attack
+type timer = {
+  curr_time : float;
+  speed : float;
+  limit : float;
+}
 
 type skill = {
+  allegiance : allegiance;
   mana_cost : int ;
   effect : effect ;
-  side : skill_side
+  regen_timer : timer;
+  tower_id : int;
+  sprite: sprite;
+  anim_timer : timer;
 }
 
 type move = {
@@ -75,7 +81,7 @@ type move = {
 
 type command =
   | Move of allegiance * int * int (* tuple of tower indices*)
-  | Skill of allegiance * skill * int
+  | Skill of skill
   | Null
 
 type mouse_state =
@@ -97,8 +103,6 @@ type button_state =
   | Depressed
   | Clicked
 
-
-
 type label_property = {
   mutable text : string;
   mutable color : color;
@@ -112,10 +116,24 @@ type button_property = {
   mutable btn_label_offset : vector2d;
 }
 
+type spell_box_state =
+  | Neutral
+  | Selected (* Depressed *)
+  | Regenerating
+  | Disabled
+
+type spell_box_property = {
+  mutable spell_box_state : spell_box_state;
+  mutable spell_box_sprite : sprite;
+  mutable spell_box_front_image : sprite option;
+  mutable spell_box_front_image_offset : vector2d;
+}
+
 type ui_element =
   | Button of button_property * vector2d * bounds * string option
   | Label of label_property * vector2d * bounds
   | Panel of sprite * vector2d * bounds
+  | SpellBox of spell_box_property * vector2d * bounds * skill
 
 type state = {
   towers : tower array;
@@ -123,6 +141,7 @@ type state = {
   player_score : int;
   enemy_score : int;
   movements : movement list;
+  player_skill : skill option;
   player_mana : int;
   enemy_mana : int
 }
