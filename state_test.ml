2c32fb107fa14d91392977ebd9b1e609ddba7e70
vagrant <vagrant@vm3110-2018sp>
2018-05-14 22:17:47 -0400

    added core types to state_test to simplify testing

--- state_test.ml
+++ state_test.ml
@@ -5,15 +5,15 @@ open State
 (* initial player movement of 5 troops with 100. speed and no progress
    going from tower 1 to tower 2.
 *)
-let init_movement12 : movement = {
-  start_tower = 1;
+let init_movement02 : movement = {
+  start_tower = 0;
   end_tower = 2;
   mvmt_troops = 5;
   mvmt_sprite = Sprite.blue_troop1_right;
   mvmt_team = Player;
   progress = 0.;
   damage = 1.;
-  speed = 50.;
+  speed = 100.;
 }
 
 let init_troop_regen_speed = 1.
@@ -27,19 +27,19 @@ let mini_max = 30.
 let troop_foot_soldier = {
   trp_type = Foot;
   trp_damage = 1.;
-  trp_speed = 50.;
+  trp_speed = 100.;
 }
 
 let troop_cavalry = {
   trp_type = Cavalry;
   trp_damage = 2.;
-  trp_speed = 100.;
+  trp_speed = 200.;
 }
 
 let base_player_tower = {
   twr_id = 0;
   twr_pos = {x = 0.; y = 50.};
-  twr_size = {w=72.; h=136.};
+  twr_size = {w = 72.; h = 136.};
   twr_sprite = Sprite.tower_base;
   twr_troop_info = troop_foot_soldier;
   twr_troops = base_init_troop_count;
@@ -100,7 +100,7 @@ let neutral_mini_cavalry_copy = {
 
 let base_enemy_tower = {
   twr_id = 2;
-  twr_pos = {x= Renderer.width -.72.;y= Renderer.height -. 136.};
+  twr_pos = {x = Renderer.width -.72.; y = Renderer.height -. 136.};
   twr_size = {w = 72.; h = 136.};
   twr_sprite = Sprite.tower_base;
   twr_troop_info = troop_foot_soldier;
@@ -116,7 +116,7 @@ let base_enemy_tower = {
 (* A copy used for immutability tests to compare against original *)
 let base_enemy_tower_copy = {
   twr_id = 2;
-  twr_pos = {x= Renderer.width -.72.;y= Renderer.height -. 136.};
+  twr_pos = {x = Renderer.width -.72.; y = Renderer.height -. 136.};
   twr_size = {w = 72.; h = 136.};
   twr_sprite = Sprite.tower_base;
   twr_troop_info = troop_foot_soldier;
@@ -137,8 +137,8 @@ let init_state : state = {
   movements = [];
   player_skill = None;
   enemy_skill = None;
-  player_mana = 0.;
-  enemy_mana = 0.;
+  player_mana = 250.;
+  enemy_mana = 250.;
 }
 
 (* A copy used for immutability tests *)
@@ -151,13 +151,13 @@ let init_state_copy : state = {
   movements = [];
   player_skill = None;
   enemy_skill = None;
-  player_mana = 0.;
-  enemy_mana = 0.;
+  player_mana = 250.;
+  enemy_mana = 250.;
 }
 
 let init_stun_from_player_valid = {
   allegiance = Player;
-  mana_cost = 0;
+  mana_cost = 100;
   effect = Stun 5.;
   regen_timer = {curr_time = 0.; speed = 1.; limit = 7.};
   tower_id = 2;
@@ -167,7 +167,7 @@ let init_stun_from_player_valid = {
 
 let init_stun_from_enemy_valid = {
   allegiance = Enemy;
-  mana_cost = 0;
+  mana_cost = 100;
   effect = Stun 5.;
   regen_timer = {curr_time = 0.; speed = 1.; limit = 7.};
   tower_id = 0;
@@ -177,8 +177,8 @@ let init_stun_from_enemy_valid = {
 
 let init_kill_from_player_valid = {
   allegiance = Player;
-  mana_cost = 0;
-  effect = Kill 5;
+  mana_cost = 70;
+  effect = Kill 15;
   regen_timer = {curr_time = 0.; speed = 1.; limit = 5.};
   tower_id = 2;
   sprite = Sprite.sprite_freeze;
@@ -187,8 +187,8 @@ let init_kill_from_player_valid = {
 
 let init_kill_from_enemy_valid = {
   allegiance = Enemy;
-  mana_cost = 0;
-  effect = Kill 5;
+  mana_cost = 70;
+  effect = Kill 15;
   regen_timer = {curr_time = 0.; speed = 1.; limit = 5.};
   tower_id = 0;
   sprite = Sprite.sprite_freeze;
@@ -229,7 +229,7 @@ let init_reg_from_enemy_valid2 = {
   allegiance = Enemy;
   mana_cost = 0;
   effect = Regen_incr 0.8;
-  regen_timer = {curr_time = 0.; speed = 1.; limit = 40.};
+  regen_timer = {curr_time = 0.; speed = 1.; limit = 20.};
   tower_id = 0;
   sprite = Sprite.sprite_freeze;
   anim_timer = {curr_time = 0.; speed = 1.; limit = 1.};
@@ -238,8 +238,122 @@ let init_reg_from_enemy_valid2 = {
 (* A fake time delta to produce test cases *)
 let delta = 0.1
 
+(* tower without [twr_sprite], [selector_offset], or [count_label_offset] *)
+type tower_core = {
+  twr_id : int;
+  twr_pos : vector2d;
+  twr_size : bounds;
+  twr_troop_info : troop;
+  twr_troops_max : float;
+  mutable twr_troops_regen_speed : float;
+  twr_team : allegiance;
+  mutable is_disabled : bool;
+}
+
+(* a movement without [mvmt_sprite] *)
+type movement_core = {
+  start_tower : int;
+  end_tower : int;
+  mvmt_troops : int;
+  mvmt_team : allegiance;
+  progress : float;
+  damage : float;
+  speed : float;
+}
+
+(* a skill without [sprite] *)
+type skill_core = {
+  allegiance : allegiance;
+  mana_cost : int;
+  effect : effect;
+  regen_timer : timer;
+  tower_id : int;
+  anim_timer : timer;
+}
+
+(* Represents the state without any information about sprites or other visual
+   elements better verifiable by playtesting.
+*)
+type state_core = {
+  towers : tower_core array;
+  num_towers : int;
+  player_score : int;
+  enemy_score : int;
+  movements : movement_core list;
+  player_skill : skill_core option;
+  enemy_skill : skill_core option;
+  player_mana : float;
+  enemy_mana : float;
+}
+
+(* Converts a tower array to a tower_core_array *)
+let tower_to_core (towers : tower array) : tower_core array =
+  Array.map (fun t ->
+      ignore (t.twr_sprite); (* OCaml doesn't know [towers] is a [tower array]
+                                unless you use something only a [tower] has. *)
+      {
+        twr_id = t.twr_id;
+        twr_pos = t.twr_pos;
+        twr_size = t.twr_size;
+        twr_troop_info = t.twr_troop_info;
+        twr_troops_max = t.twr_troops_max;
+        twr_troops_regen_speed = t.twr_troops_regen_speed;
+        twr_team = t.twr_team;
+        is_disabled = t.is_disabled
+      }
+    ) towers
+
+(* Produces a skill_core option out of a skill option *)
+let skill_to_core (skill : skill option) : skill_core option =
+  match skill with
+  | None -> None
+  | Some sk -> Some
+    begin
+      {
+        allegiance = sk.allegiance;
+        mana_cost = sk.mana_cost;
+        effect = sk.effect;
+        regen_timer = sk.regen_timer;
+        tower_id = sk.tower_id;
+        anim_timer = sk.anim_timer;
+      }
+    end
+
+(* Converts a list of movements to a list of [movement_core]s. *)
+let movements_to_core (movements : movement list) : movement_core list =
+  List.map (fun m -> ignore (m.mvmt_sprite); (* Needed for type checking *)
+      {
+        start_tower = m.start_tower;
+        end_tower = m.end_tower;
+        mvmt_troops = m.mvmt_troops;
+        mvmt_team = m.mvmt_team;
+        progress = m.progress;
+        damage = m.damage;
+        speed = m.speed;
+      }
+    ) movements
+
+(* Converts a [state] to a [state_core] *)
+let to_core (st : state) : state_core =
+  let towers_core = tower_to_core st.towers in
+  let player_skill_core = skill_to_core st.player_skill in
+  let enemy_skill_core = skill_to_core st.enemy_skill in
+  let movements_core = movements_to_core st.movements in
+  {
+    towers = towers_core;
+    num_towers = st.num_towers;
+    player_score = st.player_score;
+    enemy_score = st.enemy_score;
+    movements = movements_core;
+    player_skill = player_skill_core;
+    enemy_skill = enemy_skill_core;
+    player_mana = st.player_mana;
+    enemy_mana = st.enemy_mana;
+  }
+
+
 let helper_tests = [
-  "new_mvmt" >:: (fun _ -> assert_equal init_movement12
+  "new_mvmt" >:: (fun _ -> assert_equal init_movement02
                      (new_movement 1 2 5 Sprite.blue_troop1_right Player 1. 50.));
 ]
 
