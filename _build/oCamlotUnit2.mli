425414032872230af0cc1a0c4b66f7d7a839c524
vagrant <vagrant@vm3110-2018sp>
2018-05-14 22:17:56 -0400

    added core types to state_test to simplify testing

--- _build/oCamlotUnit2.mli
+++
@@ -1,8 +0,0 @@
-type test = string * (unit -> bool)
-
-val assert_equal : 'a -> 'a -> bool
-
-val (>::) : string -> (unit -> bool) -> test
-
-(* a list of tests against the state interface. *)
-val run_tests : test list -> string
